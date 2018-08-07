8181a4c46eda4a55b088f7ebf129cf9bfc0a7b7f
Nick Pettazzoni <pettazz@gmail.com>
2018-08-07 19:57:28 -0400

    init

---
+++ Did I Drunk ThisTests/Did_I_Drunk_ThisTests.swift
@@ -0,0 +1,36 @@
+//
+//  Did_I_Drunk_ThisTests.swift
+//  Did I Drunk ThisTests
+//
+//  Created by Nick Pettazzoni on 8/5/18.
+//  Copyright © 2018 Nick Pettazzoni. All rights reserved.
+//
+
+import XCTest
+@testable import Did_I_Drunk_This
+
+class Did_I_Drunk_ThisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
