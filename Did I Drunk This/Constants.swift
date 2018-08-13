9950867a1fd90d94bcc154fc4e12e378c04a53f0
Nick Pettazzoni <pettazz@gmail.com>
2018-08-12 21:08:30 -0400

    i am a dumbass

--- Did I Drunk This/Constants.swift
+++ Did I Drunk This/Constants.swift
@@ -12,8 +12,8 @@ struct K{
     struct Untappd{
         static let BaseUrl = "https://api.untappd.com/v4/"
         
-        static let ClientID = "86F7C2065CB32B9A1B15DE6EBFFAF0458685822E"
-        static let ClientSecret = "772C0F4DC8B119C8E0F39B1C70858B7E2FA24B30"
+        static let ClientID = "no"
+        static let ClientSecret = "lol"
         
         static let Endpoint: [String:String] = [
             "Authenticate": "https://untappd.com/oauth/authenticate/",
