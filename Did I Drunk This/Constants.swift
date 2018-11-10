227017ba5e33298007ef10f49d88d165023c6e24
Nick Pettazzoni <pettazz@gmail.com>
2018-11-09 22:03:38 -0500

    cleanup

--- Did I Drunk This/Constants.swift
+++ Did I Drunk This/Constants.swift
@@ -11,13 +11,13 @@ import Foundation
 struct K{
     struct Untappd{
         static let BaseUrl = "https://api.untappd.com/v4/"
-        
+
         static let ClientID = "no"
         static let ClientSecret = "lol"
-        
-        static let Endpoint: [String:String] = [
+
+        static let Endpoint: [String: String] = [
             "Authenticate": "https://untappd.com/oauth/authenticate/",
-            
+
             "BeerSearch": "\(K.Untappd.BaseUrl)search/beer?q=%@",
             "BeerDetails": "\(K.Untappd.BaseUrl)beer/info/%@?compact=true"
         ]
